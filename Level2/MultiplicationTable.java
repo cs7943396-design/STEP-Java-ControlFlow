7a1f5eeda1a1a74471068d35dd757511955cee32
cs7943396-design <cs7943396@gmail.com>
2026-04-06 09:00:17 +0530

    Update MultiplicationTable.java

---
+++ Level2/MultiplicationTable.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class FactorsProgram {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            for (int i = 1; i < number; i++) {
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+
+        input.close();
+    }
+}
